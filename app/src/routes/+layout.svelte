799ba39363edd7ffff4017d19a9850d17ca65a7a
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-20 21:07:46 +0000

    add video

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ 
+ <link href="https://vjs.zencdn.net/8.5.2/video-js.css" rel="stylesheet" />
+ <script src="https://vjs.zencdn.net/8.5.2/video.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/videojs-playlist/dist/videojs-playlist.min.js"></script>
+ 
+ 
+ 
 </svelte:head>
 
 
@@ -9,7 +16,9 @@
 
 <script>
     import XpostComponent from './xpost.svelte';
+    import VideoComponent from './video.svelte';
 	export let data;
+	let selected = "videos";
 	console.log(data);
 </script>
 
@@ -31,13 +40,20 @@
 </ul>
 
 
+{#if selected == "x-posts"}
+
 {#each data.xPosts as xpost}
   <svelte:component this={XpostComponent} xpost={xpost}>
   </svelte:component>
-
-
 {/each}
 
+{:else if selected == "videos"}
 
+{#each data.videos as video}
+  <svelte:component this={VideoComponent} video={video}>
+  </svelte:component>
+{/each}
 
-<slot></slot>
+{:else}
+
+{/if}
---
+++ app/src/routes/video.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let video;
+	const filebaseBase = "https://ipfs.filebase.io/ipfs";
+
+
+</script>
+
+
+<div class="card me-2 ms-2 mt-2" >
+ <video class="card-img-top video-js" controls width="100%" height="300" preload="auto" poster="fafo/IMG_0461.png">
+
+  {#if video.src == "filebase"}
+     <source src="{filebaseBase}/{video.id}" type="video/mp4">
+{:else}
+   	 <source src="{filebaseBase}" type="video/mp4">
+{/if}
+
+</video>
+
+   <div class="card-body bg-light">
+     <p class="card-text">
+
+          <span class="me-2 rounded-pill badge bg-dark">{video.title}</span>
+     
+ 
+       {#if video.date !== undefined}
+	  <span class="me-2 badge bg-dark float-end">{video.date}</span>
+{/if}
+     </p>    
+ 
+     
+       </div>
+
+</div>
