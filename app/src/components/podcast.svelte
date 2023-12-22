c168ed303d3ac6f96eb55b8da2773d5535611b29
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-22 13:12:46 +0000

    podcast copy past

--- app/src/components/podcast.svelte
+++ app/src/components/podcast.svelte
@@ -1,4 +1,72 @@
 <script>
 	export let podcast;
 	const filebaseBase = "https://ipfs.filebase.io/ipfs";
+	
+	console.log(podcast.src)
+	
+	let podcastInstance = new Howl({
+          src: [podcast.src],
+          html5: true,
+          volume: 0.9,
+          format: ["m4a"],
+          onload: function() {
+          	console.log("loading podcast ...");
+          },
+        
+          onplay: function(id) {
+          	console.log("playing podcast ...");
+          },
+        
+          onstop: function(id) {
+          	console.log("stopping podcast ...");
+          },
+          onend: function() {
+          	console.log("podcast ended");
+            Howler.unload();
+          },
+          onplayerror: function() {
+          	console.log("error playing podcast");
+            Howler.unload();
+          },
+          onloaderror: function() {
+          	console.log("error loading podcast");
+            Howler.unload();
+          }
+          
+      });
+
+
 </script>
+
+
+<div class="card me-2 ms-2 mt-2" >
+   <img id="podcast-poster-img" class="img-fluid" src="https://ipfs.filebase.io/ipfs/Qmcrww6uA7cWJAenhRwXWFJQkcCNAoMhntMCkue8aitoEV"/>
+
+
+   <div class="card-body bg-light">
+     <p class="card-text">
+
+          <span class="me-2 rounded-pill badge bg-dark">
+      
+          
+          {podcast.title}</span>
+     
+ 
+       {#if podcast.date !== undefined}
+	  <span class="me-2 badge bg-dark">{podcast.date}</span>
+       {/if}
+
+<span>
+<i class="float-end gg-play-button-o"></i>
+</span>
+
+
+
+
+
+     </p>    
+ 
+     
+       </div>
+
+</div>
