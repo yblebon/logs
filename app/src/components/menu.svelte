f2f3c81f09700c2f5a4a36cc8fcacb149e7454fa
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-23 19:13:24 +0000

    update menu

--- app/src/components/menu.svelte
+++ app/src/components/menu.svelte
@@ -7,19 +7,29 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="/">
+              <button id="home" type="button" on:click={changeContent}  class="btn text-uppercase fw-bolder text-black">Home</button>
+           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/x-posts">X-Posts</a>
+          <a class="nav-link" href="/x-posts">
+              <button id="x-posts" type="button" on:click={changeContent}  class="btn text-uppercase fw-bolder text-black">X-Posts</button>
+           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/videos">Videos</a>
+          <a class="nav-link" href="/videos">
+              <button id="videos" type="button" on:click={changeContent}  class="btn text-uppercase fw-bolder text-black">Videos</button>
+           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/podcasts">Podcasts</a>
+          <a class="nav-link" href="/podcasts">
+              <button id="podcasts" type="button" on:click={changeContent}  class="btn text-uppercase fw-bolder text-black">Podcasts</button>
+           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/articles">Articles </a>
+          <a class="nav-link" href="/articles">
+              <button id="podcasts" type="button" on:click={changeContent}  class="btn text-uppercase fw-bolder text-black">Articles</button>
+           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
