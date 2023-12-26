3e792234792a7f8d34c5d416f9ab3f3cb3c1f03d
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-26 23:22:33 +0000

    update docs

---
+++ docs/_app/immutable/chunks/paths.5GWuRT6n.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1sxz6wi?.base??"",a=globalThis.__sveltekit_1sxz6wi?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/paths.CJe1WKP1.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_3vjdn4?.base??"",a=globalThis.__sveltekit_3vjdn4?.assets??s;export{a,s as b};
