a9e97d009b3301023f2dffa4939eebc6dfff3f41
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-29 14:23:28 +0000

    init dots implementation

--- app/src/components/dots.svelte
+++ app/src/components/dots.svelte
@@ -2,8 +2,37 @@
 
 export let dots;
 
+console.log(dots)
+
 </script>
 
-{#each dots as dot}
+
+<div class="accordion" id="accordionExample">
+
+
+
+{#each dots.dots as dotsElement }
+
+<div class="mt-4 accordion-item">
+
+ <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        {dotsElement.title}
+      </button>
+    </h2>
+
+</div>
+<div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+
+
+
+
+
   
 {/each}
+
+</div>
--- app/src/routes/dots/+page.js
+++ app/src/routes/dots/+page.js
@@ -3,7 +3,6 @@ import dots from '$lib/data/dots.json';
 
 export const load = () => {
   
-  console.log(dots)
 
   return {
     dots: dots
