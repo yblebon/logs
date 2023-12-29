64533300c0be917926f07e5078dadbbc265d975d
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-29 13:39:19 +0000

    add dots menu

---
+++ app/src/components/dots.svelte
@@ -0,0 +1,9 @@
+<script>
+
+export let dots;
+
+</script>
+
+{#each dots as dot}
+  
+{/each}
---
+++ app/src/routes/dots/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+
+import DotsComponent from '../../components/dots.svelte';
+export let data;
+
+</script>
+
+{#each data.dots as dots}
+  <DotsComponent dots={dots}/>
+{/each}
