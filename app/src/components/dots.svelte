03a6cb6e887aac5a6080006020e47e4d1d4ca174
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-29 14:31:41 +0000

    init split

--- app/src/components/dots.svelte
+++ app/src/components/dots.svelte
@@ -3,6 +3,8 @@
 export let dots;
 
 console.log(dots)
+const filebaseBase = "https://ipfs.filebase.io/ipfs";
+
 
 </script>
 
@@ -24,7 +26,7 @@ console.log(dots)
 </div>
 <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+         	  <img src="{filebaseBase}/{dotsElement.src}" class="card-img-top rounded-0">
       </div>
     </div>
 
