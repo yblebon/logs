7c0c4cd6dc87e8d186189441e6e7c96654ce86b0
Yasser <135013954+yblebon@users.noreply.github.com>
2023-12-30 16:20:21 +0000

    update docs

--- app/src/components/dots.svelte
+++ app/src/components/dots.svelte
@@ -47,12 +47,24 @@ const filebaseBase = "https://ipfs.filebase.io/ipfs";
          	  <img src="{filebaseBase}/{dotsElement.src}" class="card-img-top rounded-0">
       </div>
     </div>
-
-
-
-
+    
 
   
 {/each}
+
+   <div class="card-body ps-2 pt-2 pb-2 bg-light">
+     <p class="card-text">
+     
+ 
+       {#if dots.upload_date !== undefined}
+       
+
+	  <span class="me-2 text-bg-secondary  badge">{dots.upload_date}</span>
+{/if}
+     </p>    
+ 
+     
+       </div>
+    
 
 </div>
---
+++ docs/_app/immutable/chunks/paths.cohgtOuZ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1tfhhmn?.base??"",t=globalThis.__sveltekit_1tfhhmn?.assets??s;export{t as a,s as b};
