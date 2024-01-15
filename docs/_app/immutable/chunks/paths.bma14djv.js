d062f57a483f0bbdf7fb2c14ed35136656da90ed
Yasser <135013954+yblebon@users.noreply.github.com>
2024-01-15 15:49:23 +0000

    update docs

---
+++ docs/_app/immutable/chunks/paths.bma14djv.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1llj2bi?.base??"",l=globalThis.__sveltekit_1llj2bi?.assets??s;export{l as a,s as b};
--- docs/_app/immutable/chunks/paths.q5AH-nYu.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_132x0t8?.base??"",t=globalThis.__sveltekit_132x0t8?.assets??s;export{t as a,s as b};
