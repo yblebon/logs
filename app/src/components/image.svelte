ff3b38ad1b0349475b93aaf01d3d271d9de83ed6
Yasser <135013954+yblebon@users.noreply.github.com>
2024-01-29 13:45:39 +0000

    add image pages

---
+++ app/src/components/image.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let image;
+	const filebaseBase = "https://ipfs.filebase.io/ipfs";
+	
+	const tags = ["primary", "secondary", "success", "danger", "warning", "info", "light"];
+
+
+</script>
+
+<div class="card me-4 ms-4 mt-4" >
+
+<div class="card-header text-wrap text-bg-success">
+
+     {#if image.tags !== undefined }
+     
+            {#each image.tags as tag}
+          <span class="me-2 badge float-end ">{tag}</span>
+{/each}
+           
+
+     
+     {:else}
+     
+     
+     {/if}
+     
+
+</div>
+
+   <img src="{image.url}" class="card-img-top">
+
+   <div class="card-body bg-light">
+     <p class="card-text">
+
+       {#if image.upload_date !== undefined}
+	  <span class="me-2 badge text-bg-secondary">{image.event_date}</span>
+{/if}
+     </p>    
+ 
+     
+       </div>
+
+</div>
---
+++ app/src/routes/images/[slug]/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+   import ImageComponent from '../../../components/image.svelte';
+   /** @type {import('./$types').PageData} */
+   export let data;
+   const filebaseBase = "https://ipfs.filebase.io/ipfs";
+   let og = {};
+   og["title"] = data.image.title;
+   og["description"] = data.image.title;
+   og["domain"] = "yblebon.github.io";
+   og["url"] = "https://yblebon.github.io/logs/images/"+data.image.uid
+   og["image"] = data.image.url;
+
+   
+</script>
+
+<svelte:head>
+
+  <!-- HTML Meta Tags -->
+  <title>{data.image.title}</title>
+  <meta name="description" content="">
+
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary">
+  <meta property="twitter:domain" content="{og.domain}">
+  <meta property="twitter:url" content="{og.url}">
+  <meta name="twitter:title" content="{og.title}">
+  <meta name="twitter:description" content="{og.description}">
+  <meta name="twitter:image" content="{og.image}">
+  <meta name="twitter:creator" content="@YasserLebon"/>
+
+        
+
+</svelte:head>
+
+
+<VideoComponent video={data.video}/>
