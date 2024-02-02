02b8d90225d924ca8cce65ee47ca71fd1c24e7e1
Yasser <135013954+yblebon@users.noreply.github.com>
2024-02-02 11:03:14 +0000

    init admin

---
+++ app/src/routes/admin/+page.svelte
@@ -0,0 +1,8 @@
+<div class="mb-3">
+  <label for="imageTitle" class="form-label">Title</label>
+  <input type="text" class="form-control" id="imageTitle" placeholder="image title">
+</div>
+<div class="mb-3">
+  <label for="imageFile" class="form-label"></label>
+  <input class="form-control" type="file" id="imageFile">
+</div>
