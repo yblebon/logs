51b85e3fe1161a72a02ded2335eb0351b636bb17
Yasser <135013954+yblebon@users.noreply.github.com>
2024-02-12 20:47:41 +0000

    add project

---
+++ app/src/components/project.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { base } from '$app/paths';
+	export let project;
+	const filebaseBase = "https://ipfs.filebase.io/ipfs";
+</script>
+
+
+
+
+<div class="card me-4 ms-4 mt-4" >
+
+<div class="card-header text-bg-danger">
+
+          <span class="me-2 badge text-lowercase text-wrap float-end">{project.title}</span>
+
+
+</div>
+
+
+   <div class="card-body bg-light">
+     <p class="card-text">
+     
+ 
+       {#if project.date !== undefined}
+       
+
+	  <span class="me-2 text-bg-secondary  badge">{project.date}</span>
+	  
+
+{/if}
+
+
+
+<a href="{base}/projects/{project.uid}" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">link</a>
+	  	  
+     </p>    
+ 
+     
+       </div>
+
+</div>
