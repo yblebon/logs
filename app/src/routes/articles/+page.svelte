f799f6f20cdc1f0f9a3e55e830736734b6144381
Yasser <135013954+yblebon@users.noreply.github.com>
2024-02-12 17:26:39 +0000

    update records

--- app/src/routes/articles/+page.svelte
+++ app/src/routes/articles/+page.svelte
@@ -9,6 +9,14 @@ export let data;
 
 </script>
 
+<svelte:head>
+
+  <!-- HTML Meta Tags -->
+  <title>Yasser B. Lebon Articles</title>
+  <meta name="description" content="Yasser B. Lebon article records">
+
+</svelte:head>
+
 
 {#each data.articles as article}
   <ArticleComponent article={article}/>
