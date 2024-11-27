cccba03f445759a15a983ca4a589458bb1b9eddd
yblebon <135013954+yblebon@users.noreply.github.com>
2024-11-27 21:46:54 +0000

    update docs

---
+++ docs/_app/immutable/chunks/paths.TsmoPriL.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1v4ll4l?.base??"",l=globalThis.__sveltekit_1v4ll4l?.assets??s;export{l as a,s as b};
--- docs/build/_app/immutable/chunks/paths.Gc45-h2A.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1vrrf3x?.base??"",a=globalThis.__sveltekit_1vrrf3x?.assets??s;export{a,s as b};
