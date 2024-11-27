1dbe853a26bc0ff610713f734f961a59e238419f
yblebon <135013954+yblebon@users.noreply.github.com>
2024-11-27 13:18:28 +0000

    update docs

--- docs/_app/immutable/chunks/paths.5EAQYdg8.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1lsv7g1?.base??"",a=globalThis.__sveltekit_1lsv7g1?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/paths.enxN0Lu6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ickvay?.base??"",a=globalThis.__sveltekit_1ickvay?.assets??s;export{a,s as b};
