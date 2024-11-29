c50f77d5d0ade06c84030f4e3dacbdade67bf2b9
yblebon <135013954+yblebon@users.noreply.github.com>
2024-11-29 16:06:05 +0000

    update docs

---
+++ docs/_app/immutable/chunks/paths.C_WdRip4.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_19ap0xc?.base??"",a=globalThis.__sveltekit_19ap0xc?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/paths.UK7LA1_1.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_159h7j0?.base??"",a=globalThis.__sveltekit_159h7j0?.assets??s;export{a,s as b};
