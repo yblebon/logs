ed12bc015a0422b9ae271f8cd2557047e491eac9
yblebon <135013954+yblebon@users.noreply.github.com>
2024-12-01 16:07:31 +0000

    update docs

--- docs/_app/immutable/chunks/paths.z8QID-04.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1uodlef?.base??"",e=globalThis.__sveltekit_1uodlef?.assets??s;export{e as a,s as b};
--- docs/build/_app/immutable/chunks/paths.TfKvHJUY.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_pug8yn?.base??"",a=globalThis.__sveltekit_pug8yn?.assets??s;export{a,s as b};
