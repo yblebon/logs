08c2cad1e8d0adb91823951e720da4df70770c32
yblebon <135013954+yblebon@users.noreply.github.com>
2024-12-03 17:44:22 +0000

    upadte docs

---
+++ docs/_app/immutable/chunks/paths.DPG2jvKR.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pv9u73?.base??"",a=globalThis.__sveltekit_pv9u73?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/paths.IRVM1l6s.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_3744kw?.base??"",a=globalThis.__sveltekit_3744kw?.assets??s;export{a,s as b};
