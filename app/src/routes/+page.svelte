5e4d19edbf04ef67112974dd63f26b97c77769d8
yblebon <135013954+yblebon@users.noreply.github.com>
2024-12-10 10:40:57 +0000

    update docs

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,67 +1,25 @@
 <script>
+  import XpostComponent from "../components/xpost.svelte";
+  import VideoComponent from "../components/video.svelte";
+  import ArticleComponent from "../components/article.svelte";
+  import PodcastComponent from "../components/podcast.svelte";
+  import DotsComponent from "../components/dots.svelte";
+  import ImageComponent from "../components/image.svelte";
 
-import XpostComponent from '../components/xpost.svelte';
-import VideoComponent from '../components/video.svelte';
-import ArticleComponent from '../components/article.svelte';
-import PodcastComponent from '../components/podcast.svelte';
-import DotsComponent from '../components/dots.svelte';
-import ImageComponent from '../components/image.svelte';
-
-export let data;
-
-
-
+  export let data;
 </script>
 
 <svelte:head>
-
   <!-- HTML Meta Tags -->
   <title>Yasser B. Lebon Logs</title>
-  <meta name="description" content="Yasser B. Lebon Logs personal blog, vlog, journal and containing rrecords of events">
-
+  <meta
+    name="description"
+    content="Yasser B. Lebon Logs personal blog, vlog, journal and containing rrecords of events"
+  />
 </svelte:head>
 
-
-
-
-{#each data.dataList as content }
-
-{#if content.section == "xpost"}
-	
-	  <XpostComponent xpost={content}/>
-	  
-{:else if content.section == "video"}
-
-  <VideoComponent video={content}/>
-	
-	
-{:else if content.section == "podcast"}
-
-  <PodcastComponent podcast={content}/>
-
-{:else if content.section == "article"}
-
-  <ArticleComponent article={content}/>
-  
- {:else if content.section == "dots"}
-
-  <DotsComponent dots={content}/>
-  
- {:else if content.section == "image"}
-
-  <ImageComponent image={content}/>
-
-{:else}
-
-{/if}
-
-
-
+{#each data.dataList as content}
+  {#if content.section == "podcast"}
+    <PodcastComponent podcast={content} />
+  {/if}
 {/each}
-
-
-
-
-
-
-
---
+++ docs/_app/immutable/entry/start.BWCXWtjv.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BmpUVfWK.js";export{t as start};
