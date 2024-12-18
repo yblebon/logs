2c8d0334ebd105d6fa8948a8156a46a230059d1f
Yasser <yasser.lebon@mailfence.com>
2024-12-18 20:49:45 +0100

    update card view

--- app/src/components/card.svelte
+++ app/src/components/card.svelte
@@ -7,18 +7,21 @@
   
   <div class="card mx-auto border-dark mt-4" style="max-width: 540px;">
     <div class="card-header text-bg-dark">
-        {#if card.hasOwnProperty("tags") }
-          {#each card.tags as tag}
-            <span class="me-2 badge text-bg-secondary float-start">{tag}</span>
-          {/each}
-        {/if}
-  
-      <span class="me-2 fw-bolder fs-6 text-lowercase text-wrap float-end"
+      
+     <div>
+      <span class="me-2 fw-bolder fs-6 text-lowercase text-wrap float-start"
         >{card.title}</span
       >
+     </div>
     </div>
   
     <figure>
+      {#if card.hasOwnProperty("tags") }
+          {#each card.tags as tag}
+            <span class="mx-2 mt-2 badge text-bg-secondary float-start">{tag}</span>
+          {/each}
+        {/if}
+
       <blockquote class="blockquote ps-2 pt-2">
         <p class="fs-6">{card.text}</p>
       </blockquote>
