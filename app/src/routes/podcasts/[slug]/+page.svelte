23f1f7e9de0259acd66b48da0f18a9984488bcaa
Yasser <yasser.lebon@mailfence.com>
2024-12-27 21:55:49 +0100

    update docs

--- app/src/routes/podcasts/[slug]/+page.svelte
+++ app/src/routes/podcasts/[slug]/+page.svelte
@@ -4,37 +4,7 @@
    export let data;
    const filebaseBase = "https://ipfs.filebase.io/ipfs";
    let og = {};
-   og["title"] = data.podcast.title;
-   og["description"] = data.podcast.title;
-   og["domain"] = "yblebon.github.io";
-   og["url"] = "https://yblebon.github.io/logs/podcasts/"+data.podcast.uid
-   og["audio"] = data.podcast.url;
-   og["image"] = "https://ipfs.filebase.io/ipfs/QmaxYnmU94P9J785MYkm28LpnYX5vtxuSC8tRewDn4qdCW";
    
-  
-   
-   
+     
 </script>
 
-<svelte:head>
-
-  <!-- HTML Meta Tags -->
-  <title>{data.podcast.title}</title>
-  <meta name="description" content="{data.podcast.title}">
-
-
-  <!-- Twitter Meta Tags -->
-  <meta name="twitter:card" content="summary">
-  <meta property="twitter:domain" content="{og.domain}">
-  <meta property="twitter:url" content="{og.url}">
-  <meta name="twitter:title" content="{og.title}">
-  <meta name="twitter:description" content="{og.description}">
-  <meta name="twitter:image" content="{og.image}">
-  <meta name="twitter:creator" content="@YasserLebon"/>
-
-        
-
-</svelte:head>
-
-
-<PodcastComponent podcast={data.podcast}/>
---
+++ docs/build/_app/immutable/entry/start.C2LnF8VC.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.ChOv1bLr.js";export{t as start};
