473ae01cab4419e89ecb842a89ef2361962f5f24
Yasser <yasser.lebon@mailfence.com>
2024-12-28 23:26:40 +0100

    update docs

---
+++ app/src/components/card_nav.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+    import { base } from "$app/paths";
+    export let size=20;
+  </script>
+
+
+<div class="mx-auto mt-4 h-100" style="max-width: 540px;">
+     <nav aria-label="Page navigation example">
+  <ul class="pagination pagination-sm">
+    {#each {length: size} as _, search_id}
+        <li class="page-item"><a class="page-link" href="{base}/search/{search_id + 1}">{search_id+1}</a></li>
+    {/each}
+  </ul>
+</nav>
+</div>
--- app/src/routes/cards/+page.svelte
+++ app/src/routes/cards/+page.svelte
@@ -1,19 +1,23 @@
 <script>
-
+    import { base } from "$app/paths";
     import CardComponent from '../../components/card.svelte';
+    import CardNavComponent from '../../components/card_nav.svelte';
     
     export let data;
-    
+    let size = 20;
+
     </script>
     
-    <svelte:head>
+<svelte:head>
     
       <!-- HTML Meta Tags -->
       <title>Yasser B. Lebon Cards</title>
       <meta name="description" content="Yasser B. Lebon card records">
     
-    </svelte:head>
+</svelte:head>
     
+    <CardNavComponent size={Math.ceil(data.cards.length/size)} />
+
     {#each data.cards as card}
       <CardComponent card={card}/>
     {/each}
