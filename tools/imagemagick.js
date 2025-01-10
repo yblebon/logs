077384b3645ac36c9091c77c67f8e8edcd07c68d
Yasser <yasser.lebon@mailfence.com>
2025-01-10 15:22:32 +0100

    update docs

--- tools/imagemagick.js
+++ tools/imagemagick.js
@@ -60,20 +60,14 @@ var imageToWebp = function(input_file, output_file, size, cb) {
 
 
 function processCardImage(card, output_dir) {
-  console.log(path.basename(card.image));
-  console.log(card.uid);
   const output_file = path.join(output_dir, card.uid.toString() + ".webp");
   fs.stat(output_file, (err, stats) => {
      if (err) {
-        if (card.hasOwnProperty("og_image_auto") && card.og_image_auto) {
-              let download_file = path.join("/tmp", path.basename(card.image));
-		download(card.image, download_file, function(data){
-			console.log(data);
-                        execute(download_file, output_file, card.title);
-		});
-	}
+      	let download_file = path.join("/tmp", path.basename(card.image));
+	download(card.image, download_file, function(data){
+           labelImage(download_file, output_file, card.title);
+	});
     };
- 
    });
 }
 
@@ -81,7 +75,6 @@ function convertImages(input_dir, output_dir) {
     fs.readdir(input_dir, {recursive:true}, function(err, data) {
        data.forEach(function(file){
          if ([".jpg", ".png", ".jpeg", ".avif"].indexOf(path.extname(file)) != -1 ) {
-            console.log(path.extname(file));
             let input_file = path.join(input_dir, file);
             let output_file = path.join(output_dir, path.basename(file, path.extname(file)));
             imageToWebp(input_file, output_file, "1024x1024");
@@ -91,23 +84,28 @@ function convertImages(input_dir, output_dir) {
     });  
 }
 
-var createLabels = function(jsonFile, cb){
+var createLabels = function(jsonFile, uid, cb){
   fs.readFile(jsonFile, function(err, data) { 
     if (err) throw err; 
     const cards = JSON.parse(data); 
     cards.forEach(function(card){
-      if (card.hasOwnProperty("image")) {
-           processCardImage(card, process.argv[3]);
+      if (card.hasOwnProperty("image") && card.uid.toString() === uid.toString()) {
+           processCardImage(card, "/tmp");
        }
     })
   })
 }
 
 if (process.argv.length < 3) {
-  console.error('usage: resize <input_dir> <output_dir>');
+  console.error('usage: convert <input_dir> <output_dir>');
+  console.error('       label <json_file> <uid>');
   process.exit(1);
 }
 
 if (process.argv[2] === "convert") {
    convertImages(process.argv[3], process.argv[4]);
+}
+
+else if (process.argv[2] === "label") {
+   createLabels(process.argv[3], process.argv[4]);
 }
