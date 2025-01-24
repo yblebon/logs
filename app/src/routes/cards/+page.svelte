87b433e58f61914592722dcda1d470ac04ba5dce
Yasser <yasser.lebon@mailfence.com>
2025-01-24 15:05:40 +0100

    update docs

--- app/src/routes/cards/+page.svelte
+++ app/src/routes/cards/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-    import {getData, getPageSize} from '$lib/dataloader.js';
+    import {getData, getPageSize, getBlink, getTags} from '$lib/dataloader.js';
     import { base } from "$app/paths";
     import CardComponent from '../../components/card.svelte';
     import CardNavComponent from '../../components/card_nav.svelte';
+    import TagsComponent from '../../components/tags.svelte';
     
     export let data;
 
@@ -16,6 +17,7 @@
     
 </svelte:head>
     
+    <TagsComponent tags={getTags()} blink={getBlink()}/>
     <CardNavComponent size={Math.ceil(data.cards.length/getPageSize())} />
 
     {#each data.cards as card}
---
+++ docs/_app/immutable/entry/start.9bcd8d85.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.01a05476.js";export{t as start};
