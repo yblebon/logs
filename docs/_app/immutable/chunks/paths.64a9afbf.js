5868f4fc02ae7bf8f8712b0199c4135269804706
Yasser <yasser.lebon@mailfence.com>
2025-02-18 14:27:27 +0100

    update doc

--- docs/_app/immutable/chunks/paths.64a9afbf.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_520xod)==null?void 0:s.base)??"";var a;const t=((a=globalThis.__sveltekit_520xod)==null?void 0:a.assets)??e;export{t as a,e as b};
--- docs/_app/immutable/entry/start.e4de84f6.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.227859c1.js";export{t as start};
