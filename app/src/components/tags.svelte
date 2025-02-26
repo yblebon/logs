1295b124a532f1150e630cbb2e0f21604bb518df
Yasser <yasser.lebon@mailfence.com>
2025-02-26 12:30:10 +0100

    update docs

--- app/src/components/tags.svelte
+++ app/src/components/tags.svelte
@@ -1,7 +1,7 @@
 <script>
 import { base } from "$app/paths";
 export let tags=[];
-export let blink="baguette"; 
+export let blink="duc"; 
 
 </script>
 
@@ -13,21 +13,24 @@ export let blink="baguette";
 {#each tags as tag}
 
 {#if blink == tag}
-<div class="col">
-   <button class="button-53" role="button">
-    <a href="{base}/search/tag_{tag}">
-      <span class="fs-6">{tag.toUpperCase()}</span>
-    </a>
-   </button>
+<div class="col bg-danger border border-secondary">
+ <a href="{base}/search/tag_{tag}">
 
+  <button type="button" class="btn text-white fw-bolder me-1 mb-1"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+  {tag.toUpperCase()}
+</button>
+</a>
 </div>
 {:else}
-<div class="col">
-   <button class="button-53" role="button">
-    <a href="{base}/search/tag_{tag}">
-      <span class="fs-6">{tag.toUpperCase()}</span>
-    </a>
-   </button>
+<div class="col bg-success-subtle border border-secondary">
+ <a href="{base}/search/tag_{tag}">
+
+  <button type="button" class="btn me-1 mb-1"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+  {tag.toUpperCase()}
+</button>
+</a>
 </div>
 {/if}
 
@@ -37,92 +40,3 @@ export let blink="baguette";
 </div>
 </div>
 </div>
-
-<style>
-.button-53 {
-  background-color: #3DD1E7;
-  border: 0 solid #E5E7EB;
-  box-sizing: border-box;
-  color: #000000;
-  display: flex;
-  font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-  font-size: 1rem;
-  font-weight: 700;
-  justify-content: center;
-  line-height: 1.75rem;
-  padding: .75rem 1.65rem;
-  position: relative;
-  text-align: center;
-  text-decoration: none #000000 solid;
-  text-decoration-thickness: auto;
-  width: 100%;
-  max-width: 460px;
-  position: relative;
-  cursor: pointer;
-  transform: rotate(-2deg);
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-}
-
-.button-53:focus {
-  outline: 0;
-}
-
-.button-53:after {
-  content: '';
-  position: absolute;
-  border: 1px solid #000000;
-  bottom: 4px;
-  left: 4px;
-  width: calc(100% - 1px);
-  height: calc(100% - 1px);
-}
-
-.button-53:hover:after {
-  bottom: 2px;
-  left: 2px;
-}
-
-@media (min-width: 768px) {
-  .button-53 {
-    padding: .5rem 1rem;
-    font-size: 1rem;
-  }
-}
-
-.button-89 {
-  --b: 3px;   /* border thickness */
-  --s: .45em; /* size of the corner */
-  --color: #373B44;
-  
-  padding: calc(.5em + var(--s)) calc(.9em + var(--s));
-  color: var(--color);
-  --_p: var(--s);
-  background:
-    conic-gradient(from 90deg at var(--b) var(--b),#0000 90deg,var(--color) 0)
-    var(--_p) var(--_p)/calc(100% - var(--b) - 2*var(--_p)) calc(100% - var(--b) - 2*var(--_p));
-  transition: .3s linear, color 0s, background-color 0s;
-  outline: var(--b) solid #0000;
-  outline-offset: .6em;
-  font-size: 16px;
-
-  border: 0;
-
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-}
-
-.button-89:hover,
-.button-89:focus-visible{
-  --_p: 0px;
-  outline-color: var(--color);
-  outline-offset: .05em;
-}
-
-.button-89:active {
-  background: var(--color);
-  color: #fff;
-}
-</style>
---
+++ docs/_app/immutable/entry/start.0f5b6551.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.f86b1e35.js";export{t as start};
